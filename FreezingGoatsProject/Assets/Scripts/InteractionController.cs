bf4e4b533df474e96f24c3bad2bb11bb8bf7248d
Kjell <the_underpro@gmx.net>
2019-12-07 10:41:35 +0100

    aktueller stand

--- FreezingGoatsProject/Assets/Scripts/InteractionController.cs
+++ FreezingGoatsProject/Assets/Scripts/InteractionController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class InteractionController : MonoBehaviour
 {
-   public AudioSource honkSound;
+    public AudioSource honkSound;
+    [SerializeField] private LayerMask m_WhatIsYeetable;
+    const float k_yeetableRadius = .5f;
+    [SerializeField] private Transform m_GroundCheck;
 
     // Start is called before the first frame update
     void Start()
     {
         honkSound = GetComponent<AudioSource>();
-        honkSound.Play();
     }
 
     public void Honk(bool isHonking)
@@ -22,11 +24,28 @@ public class InteractionController : MonoBehaviour
 
     }
 
-    
-
-    // Update is called once per frame
-    void Update()
+    public void Yeet(bool isYeeting)
     {
-       
+        if (isYeeting)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_yeetableRadius, m_WhatIsYeetable);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject != gameObject)
+                {
+                    isYeeting = true;
+                    gameObject.layer.CompareTo(i);
+                    
+
+
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
