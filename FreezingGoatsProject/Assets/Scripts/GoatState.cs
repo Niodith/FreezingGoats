4584f5f3c0bee5e7cf0df33fbf9e7eb5f46ead38
Kjell <the_underpro@gmx.net>
2019-12-07 18:09:58 +0100

    PhysicsAreDoneBiatches
    see title

---
+++ FreezingGoatsProject/Assets/Scripts/GoatState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatState : MonoBehaviour
+{
+    public int hitpoints = 12;
+    public GameObject iceBlock;
+
+   public void headButtEvent(GameObject kicker)
+    {
+        hitpoints--;
+        if (hitpoints <= 0)
+        {
+            DIE();
+        }
+    }
+
+    public void DIE()
+    {
+        iceBlock.SetActive(true);
+        InputController killme = gameObject.GetComponent<InputController>(); 
+        GoatController killme2 = gameObject.GetComponent<GoatController>();
+        if (killme != null) Destroy(killme);
+        if (killme2 != null) Destroy(killme2);
+    }
+}
