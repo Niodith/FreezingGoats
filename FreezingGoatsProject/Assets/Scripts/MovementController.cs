2e525a814e609c2d42b3ff908a2f8b3196850347
Feli <fm.hank@web.de>
2019-12-07 02:57:54 +0100

    First try of movement controller -WIP

---
+++ FreezingGoatsProject/Assets/Scripts/MovementController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+
+    public float speed;                //Floating point variable to store the player's movement speed.
+    public float moveHorizontal;
+    public bool jump;
+    public GoatController goatController;
+
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        moveHorizontal = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+       
+    }
+    void FixedUpdate()
+    {
+        goatController.Move(moveHorizontal * Time.fixedDeltaTime, jump);
+       Debug.Log(moveHorizontal);
+        jump = false;
+      
+    }
+}
